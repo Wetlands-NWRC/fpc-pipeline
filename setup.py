2687aa28a5b3d34857c823eaf47e90966feee1cc
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-01-26 19:19:03 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup
 
-setup()
+setup(
+    name='FPCPipeline',
+    author='Ryan Hamilton',
+    scripts=['bin/run-main.sh', 'bin/run-diagnostics.sh'],
+    version='1.0.0'
+)
