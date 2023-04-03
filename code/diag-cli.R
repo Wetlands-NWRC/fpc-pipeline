e297695ac7aebdc77754e4ad2e46ecc02efc6b00
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-04-03 10:23:19 -0400

    added daignostics cli interface for R

---
+++ code/diag-cli.R
@@ -0,0 +1,18 @@
+command.arguments <- commandArgs(trailingOnly = TRUE);
+data.directory    <- normalizePath(command.arguments[1]);
+code.directory    <- normalizePath(command.arguments[2]);
+output.directory  <- normalizePath(command.arguments[3]);
+target.variable   <- command.arguments[4];
+target.landcovers <- command.arguments[5];
+
+print( data.directory );
+print( code.directory );
+print( output.directory );
+print( target.variable );
+
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+start.proc.time <- proc.time();
+
+# set working directory to output directory
+setwd( output.directory );
